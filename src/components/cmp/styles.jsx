ee3a51c473a0e5934331b4d53a9e21904e5d1672
huy <huynha@vng.com.vn>
2023-11-17 17:04:48 +0700

    update font size

--- src/components/cmp/styles.jsx
+++ src/components/cmp/styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const CmpWrapper = styled.div`
   color: #BF4F74;
-  font-size: 1em;
+  font-size: 100%;
   margin: 1em;
   padding: 0.25em 1em;
   border: 2px solid #BF4F74;
